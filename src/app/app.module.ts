b27adef35a1fc8c47e1357dde9048ffd5032e40b
Silas Dantas <silas-dantas@hotmail.com>
2020-10-28 01:41:06 -0300

    first commit demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SeletonLoaderModule } from './seleton-loader/seleton-loader.module'
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SeletonLoaderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/seleton-loader/seleton-loader.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seleton-loader',
+  template: `
+    <div [ngStyle]="getMyStyles()" class="skelt-load loader"></div>
+  `,
+  styleUrls: ['./seleton-loader.component.css']
+})
+export class SeletonLoaderComponent implements OnInit {
+  @Input() Cwidth;
+  @Input() Cheight;
+  @Input() circle: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getMyStyles() {
+    const myStyles = {
+      'width.px': this.Cwidth ? this.Cwidth : '',
+      'height.px': this.Cheight ? this.Cheight : '',
+      'border-radius': this.circle ? '50%' : ''
+    }
+    return myStyles;
+  }
+
+}
